e133705d408c67c2acc99ad05cae9e750b0d02cd
Forsbakka <mariafors1234@gmail.com>
2023-10-01 14:54:02 +0300

    Добавьте файлы проекта.

---
+++ testingApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testingApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Студентка группы 3-09ПС-1 Форсбакка Мария");
+        }
+    }
+}
